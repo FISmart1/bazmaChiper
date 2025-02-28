42c1f93d31e273f65e8907b8cb98dd8cf7703917
Reev-Dev <attarrifaismktibazma@gmail.com>
2025-02-28 15:07:11 +0700

    tambahkan halaman awalan masuk

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,13 +2,18 @@ import React, { useContext } from 'react';
 
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import Awal from "../views/dahboards/awal"
+
 import CaesarCipher from "../views/home/index"
 
 export default function AppRoutes() {
     return (
         <Routes>
             {/* route "/" */}
-            <Route path="/" element={<CaesarCipher />} />
+
+            <Route path="/" element={<Awal />} />
+
+            <Route path="/chiper" element={<CaesarCipher />} />
 
         </Routes>
     );
