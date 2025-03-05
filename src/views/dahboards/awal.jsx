9577cb5a5d6dabe820f63abf24e8be378841688f
Reev-Dev <attarrifaismktibazma@gmail.com>
2025-03-05 09:23:46 +0700

    ukuran judul

--- src/views/dahboards/awal.jsx
+++ src/views/dahboards/awal.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Awal() {
     return (
         <div className="container Awal">
-            <img src={logo} alt="Logo" width="800px" height="220" className="me-2" />
+            <img src={logo} alt="Logo" width="400px" height="120" className="me-2" />
             <p className="col-md-12 fs-4 text-black">Selamat datang di website Bazma Chiper.</p>
             <Link to="/chiper" className="btn btn-lg me-3 btn-link">LANGSUNG GAS</Link>
         </div>
